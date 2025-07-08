2fe2e723c1f5d907f5e0d6028430a645fd8f8936
nidulaj <nidulajayalath@gmail.com>
2025-07-08 11:10:01 +0530

    user registration done

--- contactlist-backend/controllers/authController.js
+++ contactlist-backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const {findUserByCredentials} = require('../models/userModel')
+const {findUserByCredentials, createUser, findUserByEmail} = require('../models/userModel')
 
 const login = async (req, res) => {
     const {username, password} = req.body
@@ -18,4 +18,23 @@ const login = async (req, res) => {
     }
 };
 
-module.exports = {login}
+const register = async (req, res) => {
+    const {firstName, lastName, email, username, password} = req.body
+
+    try{
+        const existingUser = await findUserByEmail(email)
+        if(existingUser){
+            return res.status(409).json({message: "Email is already registered. Try another email."})
+        }
+
+        const newUser = await createUser(firstName, lastName, email, username, password)
+        res.status(201).json({message: "User registered successfully", user : newUser})
+    } catch(err){
+        console.error('Register Error:', err); // log error to console
+        res.status(500).json({error: "Server error"})
+    }
+}
+
+
+
+module.exports = {login, register}
