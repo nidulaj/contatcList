76b92c83873dfc84acb137b538a5dbed9f44ea91
nidulaj <nidulajayalath@gmail.com>
2025-07-14 16:13:34 +0530

    user profile delete done

--- contactlist-frontend/src/pages/UserProfile.jsx
+++ contactlist-frontend/src/pages/UserProfile.jsx
@@ -1,41 +1,83 @@
-import React from 'react';
-import axios from 'axios';
-import EditUserProfile from '../components/EditUserProfile';
+import React from "react";
+import axios from "axios";
+import EditUserProfile from "../components/EditUserProfile";
+import Swal from "sweetalert2";
+import { handleLogout } from "../utils/logout";
+import { useNavigate } from "react-router-dom";
+import DeleteAlert from "../components/DeleteAlert";
 
-export default function UserProfile(){
-    const [userData, setUserData] = React.useState(null)
+export default function UserProfile() {
+  const [userData, setUserData] = React.useState(null);
 
-    const fetchUserData = () => {
-        const token = localStorage.getItem("accessToken")
+  const navigate = useNavigate();
+  const fetchUserData = () => {
+    const token = localStorage.getItem("accessToken");
 
-        axios.get("http://localhost:5000/auth/profile", {
-            headers: {Authorization: `Bearer ${token}`}
-        })
-        .then((res) => {
-            setUserData(res.data.message)
-        })
-        .catch((err) => {
-             console.error("Profile load error:", err);
-             alert("Failed to load user profile.");
-        })
+    axios
+      .get("http://localhost:5000/auth/profile", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setUserData(res.data.message);
+      })
+      .catch((err) => {
+        console.error("Profile load error:", err);
+        alert("Failed to load user profile.");
+      });
+  };
+  React.useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const handleDelete = async () => {
+    const token = localStorage.getItem("accessToken");
+
+    try {
+      await axios.delete(`http://localhost:5000/auth/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      Swal.fire({
+        title: "Success!",
+        text: "Profile was Deleted successfully.",
+        icon: "success",
+        confirmButtonText: "OK",
+      }).then(() => handleLogout(navigate));
+    } catch (err) {
+      console.error("Delete error:", err.response?.data || err.message);
+      Swal.fire({
+        title: "Oops...!",
+        text: "Something went wrong!",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
     }
-    React.useEffect(() => {
-      fetchUserData()
-    } , [])
-    return(
+  };
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {userData ? (
         <div>
-            <h1>User Profile</h1>
-            {userData ? (
-        <div>
-          <p><strong>First Name:</strong> {userData.firstName}</p>
-          <p><strong>Last Name:</strong> {userData.lastName}</p>
-          <p><strong>Email:</strong> {userData.email}</p>
-          <p><strong>Username:</strong> {userData.username}</p>
-          <EditUserProfile userData = {userData} onUpdate = {fetchUserData}/>
+          <p>
+            <strong>First Name:</strong> {userData.firstName}
+          </p>
+          <p>
+            <strong>Last Name:</strong> {userData.lastName}
+          </p>
+          <p>
+            <strong>Email:</strong> {userData.email}
+          </p>
+          <p>
+            <strong>Username:</strong> {userData.username}
+          </p>
+          <EditUserProfile userData={userData} onUpdate={fetchUserData} />
+          <DeleteAlert handleDelete={handleDelete} type="Profile" />
         </div>
       ) : (
         <p>Loading user data...</p>
       )}
-        </div>
-    )
+    </div>
+  );
 }
